7710f442a1cbf9574c7a41eff4e4bce9f117399a
Edmilson Ferreira <100481668+Edmilson95@users.noreply.github.com>
2022-08-05 20:09:33 -0300

    praticando javascript

---
+++ aula11/ex010.js
@@ -0,0 +1,8 @@
+var vel = 61;
+console.log(`a velocidade do seu carro é ${vel}km/h`)
+if (vel > 60){
+    console.log('Você ultrapassoou a velocidade permitida. MULTADO!')
+} else {
+    console.log('Dirija sempre usando o cinto de segurança')
+}
+// indentação

63334636eb57beefadfdc0b24e1cf83303ba055a
Edmilson Ferreira <100481668+Edmilson95@users.noreply.github.com>
2022-08-15 15:35:12 -0300

    exercicios de javascript

--- aula13/ambiente.js
+++ aula13/ambiente.js
@@ -1,4 +1,4 @@
-var c = 1;
+/*var c = 1;
 while (c <= 6) {
     console.log(`Passo ${c}`)
     c++;
@@ -9,4 +9,11 @@ var c = 1
 do {
     console.log(`Passo ${c}`)
     c++
-} while (c <= 6)
+} while (c <= 6)*/
+
+console.log('vai começar')
+
+for (var c=1; c <=4; c++){
+    console.log(c)
+}
+console.log('FIM')
